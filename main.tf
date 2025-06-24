9379085451463a705d8bcc3bbb965d8041d1f207
alina-ruslan <alina.rkyzy@gmail.com>
2025-06-23 20:23:02 -0700

    there is a new change in main

--- main.tf
+++ main.tf
@@ -2,3 +2,12 @@
 resource "aws_instance" "name" {
  
 }
+
+resource "aws_instance" "example" {
+  ami           = "ami-12345678" # Replace with a valid AMI ID
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleInstance"
+  }
+}
